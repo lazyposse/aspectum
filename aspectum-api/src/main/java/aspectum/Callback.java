1765de1f25836ace0d4d4ed5000472d089b7f17a
Denis Labaye <denis.labaye@gmail.com>
2012-10-03 08:06:32 +0200

    DONE refactor api java code

--- aspectum-api/src/main/java/aspectum/Callback.java
+++ aspectum-api/src/main/java/aspectum/Callback.java
@@ -1,13 +1,7 @@
 package aspectum;
 
 public interface Callback {
-	@SuppressWarnings("rawtypes")
-	void before(Thread t, Class type, String methodName, Object[] args);
-
-	@SuppressWarnings("rawtypes")
-	void afterReturning(Thread t, Class type, String methodName,
-			Object returnValue);
-
-	@SuppressWarnings("rawtypes")
-	void afterThrowing(Thread t, Class type, String methodName, Throwable th);
+    void before         (Thread t, Class type, String methodName, Object[]  args        );
+    void afterReturning (Thread t, Class type, String methodName, Object    returnValue );
+    void afterThrowing  (Thread t, Class type, String methodName, Throwable th          );
 }
