19b3cab967689137fe350b440a477a12e174aec3
Denis Labaye <labaye.d@sfeir.com>
2012-10-27 12:03:31 +0200

    DONE A script to run aspectum on the user application

--- aspectum-module/src/test/run-test-time-trace.sh
+++ aspectum-module/src/test/run-test-time-trace.sh
@@ -3,14 +3,12 @@
 HOME="$(readlink -f "$(dirname "$0")")"/../../..
 cd $HOME
 
-ASPECTUM_VERSION=0.0.2
-
 mvn clean install dependency:copy-dependencies
 
 CP=aspectum-module/target/test-classes
-CP+=:aspectum-core/target/aspectum-core-$ASPECTUM_VERSION.jar
-CP+=:aspectum-module/target/aspectum-module-$ASPECTUM_VERSION.jar
-CP+=:aspectum-api/target/aspectum-api-$ASPECTUM_VERSION.jar
+CP+=:aspectum-core/target/*
+CP+=:aspectum-module/target/*
+CP+=:aspectum-api/target/*
 
 AGENT=./aspectum-module/target/dependency/aspectjweaver-1.7.0.jar
 
@@ -18,4 +16,3 @@ java -javaagent:"$AGENT"                                    \
     -cp "$CP"                                               \
     -Daspectum.callback=aspectum.module.MethodTimeCallback  \
     aspectum.sampleapp.SampleApplication
-
